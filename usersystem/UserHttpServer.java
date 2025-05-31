90f062220afe536e2fe26fd53f384dea86ff1f53
yeverycode <ktyjj030677@gmail.com>
2025-05-31 21:04:45 +0900

    Update UserHttpServer.java

--- usersystem/UserHttpServer.java
+++ usersystem/UserHttpServer.java
@@ -142,7 +142,7 @@ public class UserHttpServer {
     static Connection getConnection() throws SQLException, ClassNotFoundException {
         String url = "jdbc:mysql://localhost:3306/java_studyroom_project?serverTimezone=UTC&characterEncoding=UTF-8";
         String dbUser = "root";
-        String dbPassword = "0327";
+        String dbPassword = "";
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(url, dbUser, dbPassword);
     }
